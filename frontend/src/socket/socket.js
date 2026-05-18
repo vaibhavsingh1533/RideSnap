03e4c0a1000d81bcfc8435ae2255ceeac0e297f6
Vaibhav singh <vaibhavpalsingh1234@gmail.com>
2026-05-19 01:51:24 +0530

    frontend env added

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.http://localhost:5000, {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket"],
   withCredentials: true
 });
